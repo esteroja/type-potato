a30fc08381ec55a960064c159db369e9e01654b4
Ester Ojala <146342718+esteroja@users.noreply.github.com>
2024-09-19 15:01:56 +0300

    feat: added prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import MyButton from './components/MyButton';
-import MyForm from './components/MyForm';
-import Login from './components/Login';
-import { Outlet } from 'react-router-dom';
-
+import React from "react"
+import logo from "./logo.svg"
+import "./App.css"
+import MyButton from "./components/MyButton"
+import MyForm from "./components/MyForm"
+import Login from "./components/Login"
+import { Outlet } from "react-router-dom"
 
 function App() {
   return (
     <>
-    {/* <MyButton/> */}
-    {/* <MyForm/> */}
-    {/*<Login/>*/}    
-<h1>Header</h1>
-    <Outlet/>
+      {/* <MyButton/> */}
+      {/* <MyForm/> */}
+      {/*<Login/>*/}
+      <h1>Header</h1>
+      <Outlet />
     </>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,21 +1,20 @@
-import { Box, Button, Stack, TextField, Typography } from "@mui/material";
-import React, { FormEvent, useEffect } from "react";
-import { useState } from "react";
+import { Box, Button, Stack, TextField, Typography } from "@mui/material"
+import React, { FormEvent, useEffect } from "react"
+import { useState } from "react"
 
+export default function MyForm() {
+  const [form, setForm] = useState({ username: "", password: "" })
+  //const [username, setUsername] = useState("");
+  //const [password, setPassword] = useState("");
 
-export default function MyForm(){
-    const [form, setForm] = useState({username: "", password: ""});
-    //const [username, setUsername] = useState("");
-    //const [password, setPassword] = useState("");
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    console.log(form)
 
-    const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
-        console.log(form)
+    // console.log({username, password});
+  }
 
-        // console.log({username, password});
-    };
-
-/*     useEffect(()=>{
+  /*     useEffect(()=>{
         console.log("Renders every time");
     });
 
@@ -27,40 +26,60 @@ export default function MyForm(){
         console.log("Renders every time username changes");
     }, [username]); */
 
-    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-/*         setForm(prevForm => {
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    /*         setForm(prevForm => {
             return{
 
             }
         }) */
-        setForm(prevForm => ({
-            ...prevForm,
-            [event.target.name]: event.target.value
-        }))
-    };
+    setForm(prevForm => ({
+      ...prevForm,
+      [event.target.name]: event.target.value,
+    }))
+  }
 
-    return (
-        <Box sx={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            height: "100vh",
-          }}>
-            <form onSubmit={handleSubmit}> 
-            <Stack gap={2}>
-            <Typography variant="h6" gutterBottom align="center">
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+      }}
+    >
+      <form onSubmit={handleSubmit}>
+        <Stack gap={2}>
+          <Typography
+            variant="h6"
+            gutterBottom
+            align="center"
+          >
             Login
-            </Typography>
-                <TextField name="username" id="outlined-basic" label="username" variant="outlined" 
-                    // onChange={(event)=> setUsername(event.target.value)}/>
-                    onChange={handleChange}></TextField>
-                <TextField name="password" id="outlined-basic" label="password" 
-                    //onChange={(event)=> setPassword(event.target.value)}/>
-                    onChange={handleChange}></TextField>
-    
-                <Button variant="contained" type="submit">Saada</Button>
-            </Stack>    
-            </form>
-        </Box>
-      );
+          </Typography>
+          <TextField
+            name="username"
+            id="outlined-basic"
+            label="username"
+            variant="outlined"
+            // onChange={(event)=> setUsername(event.target.value)}/>
+            onChange={handleChange}
+          ></TextField>
+          <TextField
+            name="password"
+            id="outlined-basic"
+            label="password"
+            //onChange={(event)=> setPassword(event.target.value)}/>
+            onChange={handleChange}
+          ></TextField>
+
+          <Button
+            variant="contained"
+            type="submit"
+          >
+            Saada
+          </Button>
+        </Stack>
+      </form>
+    </Box>
+  )
 }
